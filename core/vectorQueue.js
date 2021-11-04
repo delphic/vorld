1576fe6fc1b0702633603bd6be9820ae9f9aa616
delphic <delphic.uk@gmail.com>
2021-11-04 23:47:04 +0000

    Maybe don't allow dupes in your BFS

--- core/vectorQueue.js
+++ core/vectorQueue.js
@@ -7,16 +7,21 @@ let VectorQueue = module.exports = (function(){
 
 		let index = 0;
 		let vectors = [];
+		let keys = {};
 		queue.push = (x, y, z) => {
 			let targetIndex = index + queue.length;
-			if (vectors.length <= targetIndex) {
-				vectors.push([x, y, z]);
-			} else {
-				vectors[targetIndex][0] = x;
-				vectors[targetIndex][1] = y;
-				vectors[targetIndex][2] = z;
+			let key = x + "_" + y + "_" + z;
+			if (!keys[key]) {
+				if (vectors.length <= targetIndex) {
+					vectors.push([x, y, z]);
+				} else {
+					vectors[targetIndex][0] = x;
+					vectors[targetIndex][1] = y;
+					vectors[targetIndex][2] = z;
+				}
+				keys[key] = true;
+				queue.length += 1;
 			}
-			queue.length += 1;
 		};
 		queue.pushVector = (vec) =>{
 			queue.push(vec[0], vec[1], vec[2]);
@@ -28,11 +33,13 @@ let VectorQueue = module.exports = (function(){
 			let vector = vectors[index];
 			index += 1;
 			queue.length -= 1;
+			keys[vector[0] + "_" + vector[1] + "_" + vector[2]] = false;
 			return vector;
 		};
 		queue.reset = () => {
 			index = 0;
 			queue.length = 0;
+			keys = {};
 		};
 
 		return queue;
