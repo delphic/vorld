1a8d78c2c2b2b7b710ea7e66ae1236adc7773a4e
delphic <delphic.uk@gmail.com>
2021-11-14 11:13:08 +0000

    Added support for a worker tracking id to lighting worker.

--- lighting/worker.js
+++ lighting/worker.js
@@ -3,18 +3,19 @@ let Vorld = require('../core/vorld');
 
 onmessage = function(e) {
 
+	let id = e.data.id;
 	let vorld = e.data.vorld;
 	let bounds = e.data.bounds;
 	let count = 0, total = Object.keys(vorld.heightMap).length;
 
 	Lighting.performLightingPass(vorld, bounds, () => {
 		count++;
-		this.postMessage({ progress: count / total });
+		this.postMessage({ id: id, progress: count / total });
 	});
 
 	if (bounds) {
 		// Reduce result into bounds requested
 		vorld = Vorld.createSlice(vorld, bounds.iMin, bounds.iMax, bounds.jMin, bounds.jMax, bounds.kMin, bounds.kMax);
 	}
-	this.postMessage({ complete: true, vorld: vorld });
+	this.postMessage({ id: id, complete: true, vorld: vorld });
 };
